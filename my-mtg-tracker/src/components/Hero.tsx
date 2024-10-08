7dfb1537fdbca371c13c6c661027096d2c00f82b
PocketsDK <pocketsdk@outlook.com>
2024-10-08 13:33:02 -0600

    removed bottom margin from hero component

--- my-mtg-tracker/src/components/Hero.tsx
+++ my-mtg-tracker/src/components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = ({
     title="My MTG Tracker"
 }) => {
   return (
-    <section className="bg-gray-700 py-20 mb-4">
+    <section className="bg-gray-700 py-20">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <div className="text-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
