2f41797023787c8dd75f5d7737cd636d6cec74d7
PocketsDK <pocketsdk@outlook.com>
2024-10-09 09:34:31 -0600

    Playing around ith colors

--- my-mtg-tracker/src/components/Decks.tsx
+++ my-mtg-tracker/src/components/Decks.tsx
@@ -1,36 +1,35 @@
-import testData from './testData.json';
-import { Deck } from './Deck';  
+import testData from "./testData.json";
+import { Deck } from "./Deck";
 
 export interface DecksProps {
-    deck: typeof Deck[];
+  deck: (typeof Deck)[];
 }
 
 const Decks = () => {
+  const recentDecks = testData.slice(0, 3);
 
-    const recentDecks = testData.slice(0, 3)
-
-    return (
-        <section className="bg-orange-600 px-4 py-10">
-            <div className="container-xl lg:container m-auto">
-                <h2 className="text-3xl font-bold text-white mb-6 text-center">
-                    Recent Decks
-                </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentDecks.map((deckData) => (
-                        <Deck 
-                            deckID={deckData.deckID}
-                            deckName={deckData.deckName}
-                            deckType={deckData.deckType}
-                            deckColors={deckData.deckColors}
-                            deckVersion={deckData.deckVersion}
-                            winRate={deckData.winRate}
-                            gamesPlayed={deckData.gamesPlayed}
-                        />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-orange-500 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-white mb-6 text-center">
+          Recent Decks
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {recentDecks.map((deckData) => (
+            <Deck
+              deckID={deckData.deckID}
+              deckName={deckData.deckName}
+              deckType={deckData.deckType}
+              deckColors={deckData.deckColors}
+              deckVersion={deckData.deckVersion}
+              winRate={deckData.winRate}
+              gamesPlayed={deckData.gamesPlayed}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Decks;
--- my-mtg-tracker/src/components/NavBar.tsx
+++ my-mtg-tracker/src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 const NavBar = () => {
   return (
-    <nav className="bg-gray-900 p-4">
-        <ul className="text-orange-500 flex space-x-4">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#decks">Decks</a></li>
-            <li><a href="#about">About</a></li>
-        </ul>
+    <nav className="bg-gray-700 p-4">
+      <ul className="text-orange-500 flex space-x-4">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#decks">Decks</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
