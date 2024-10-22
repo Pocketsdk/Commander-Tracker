bcdae0134a8bc90d857cdfdd64e2225e0834f4b4
PocketsDK <pocketsdk@outlook.com>
2024-10-22 09:29:43 -0600

    tweaked formatting, titles and colors for NavBar and Hero (hero blank for now, may add search, add etc as quick options later.

--- my-mtg-tracker/src/components/NavBar.tsx
+++ my-mtg-tracker/src/components/NavBar.tsx
@@ -1,14 +1,19 @@
-const NavBar = () => {
+const NavBar = ({ title = "My MTG Tracker" }) => {
   return (
     <nav className="bg-gray-700 p-4">
-      <ul className="text-orange-500 flex space-x-4">
-        <li className="hover:text-black">
+      <ul className="text-white flex items-center space-x-4">
+        <li style={{ width: "10%", height: "10%" }}>
+          <a href="/">
+            <img src="src\assets\PDKLogo-cropped.png" />
+          </a>
+        </li>
+        <li className="hover:text-orange-600">
           <a href="/">Home</a>
         </li>
-        <li className="hover:text-black">
+        <li className="hover:text-orange-600">
           <a href="#decks">Decks</a>
         </li>
-        <li className="hover:text-black">
+        <li className="hover:text-orange-600">
           <a href="#about">About</a>
         </li>
       </ul>
