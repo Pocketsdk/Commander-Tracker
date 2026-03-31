12691e0d3d91f7f7f435721d8841c31df3ffe494
PocketsDK <pocketsdk@outlook.com>
2026-03-31 14:20:20 -0600

    backup

--- my-mtg-tracker/src/components/__tests__/Hero.test.tsx
+++ my-mtg-tracker/src/components/__tests__/Hero.test.tsx
@@ -1,12 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
-import Hero from '../Hero';
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import Hero from "../Hero";
+import "@testing-library/jest-dom";
+import React from "react";
 
-describe('Hero', () => {
-    test('renders correctly', () => {
-        render(<Hero />);
-        const heroElement = screen.getByRole('heading', { name: /My MTG Tracker/i });
-        expect(heroElement).toBeDefined();
-    });
+describe("Hero", () => {
+  test("renders correctly", () => {
+    render(<Hero />);
+    const heroElement = screen.getByText(
+      "Search and other functions coming soon..."
+    );
+    expect(heroElement).toBeDefined();
+  });
 });
